d68c4b8c5bccd6b7b730e8489b3a1f6a32c020d9
Miloepeng <yeoyongsheng2003@gmail.com>
2025-08-19 12:20:39 +0800

    Add Echo feature to chatbot

--- src/main/java/Ego.java
+++ src/main/java/Ego.java
@@ -1,10 +1,39 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Ego {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        echo();
+    }
+
+    public static void line() {
+        System.out.println("________________________________________________________\n");
+    }
+
+    public static void greet() {
+        line();
+        String greeting = "Hello there diamonds in the rough, I'm Ego *smiles*\n" +
+                "How may I be of assistance to your improvement today?\n";
+        System.out.println(greeting);
+        line();
+    }
+
+    public static void bye() {
+        line();
+        String bye = "Farewell... see you soon";
+        System.out.println(bye);
+        line();
+    }
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            line();
+            System.out.println(input + "\n");
+            line();
+            input = scanner.nextLine();
+        }
+        bye();
     }
 }
